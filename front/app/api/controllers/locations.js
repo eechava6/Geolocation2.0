aee22eb9f7d0ddb48c672e79f4b30bcff537b9ca
Esteban <eechava6@eafit.edu.co>
2019-06-03 15:46:12 -0500

    Locations microservice - Added static petitions to Locaitons microserve, Tested and works OK

--- front/app/api/controllers/locations.js
+++ front/app/api/controllers/locations.js
@@ -1,6 +1,9 @@
 //Fs reads a file to later write it to user
 const fs = require('fs');
 
+//Axios to make HTTP asynchronous request
+const axios = require('axios')
+
 module.exports = {
 
 //Save a new location, user just send a post request and 
@@ -9,7 +12,15 @@ module.exports = {
    if(!req.session.data) {
       res.redirect('/')
     } else {
-      console.log("save")
+      axios.post('http://localhost:4000/saveLocation', {
+         username: req.session.data.username, 
+         trackId:req.body.trackId,
+         latitude: req.body.latitude, 
+         longitude: req.body.longitude, 
+      }).then(response => {
+        
+        return res.json(response.data);
+      })
     }
      
  },
@@ -17,17 +28,27 @@ module.exports = {
  //Search all locations related to a user, user just send post request
  //and system determines username and calls DB finding his routes history
  search: async(req, res, next) =>{
-
-   console.log("search")
+    axios.post('http://localhost:4000/searchLocations', {
+    username: req.session.data.username, }).then(response =>  {
+    return res.json(response.data);
+  })
  },
 
 //Given a track name return all latitudes and longitudes saved with that track ID
  filter: async(req, res, next) =>{
-   console.log("Filter")
+  axios.post('http://localhost:4000/mapLocations', {
+      trackId:req.body.trackId,
+  }).then(response =>  {
+    return res.json(response.data);
+  })
 },
  //Clear all locations related to a user, user just send post request
  //and system determines username and calls DB removing his history
  clear: async(req,res,next) =>{
-   console.log("clear")
+    axios.post('http://localhost:4000/clearLocations', {
+      username:req.session.data.username,
+    }).then(response =>  {
+      return res.json(response.data);
+    })
  }
 }
